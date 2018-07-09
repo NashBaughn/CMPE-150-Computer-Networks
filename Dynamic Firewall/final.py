6042510b2cd80f4bf321bd76531980883ecdcd7b
NashBaguhn <nbaughn@ucsc.edu>
2018-07-09 09:13:55 -0700

    removed uselss comments

--- Dynamic Firewall/final.py
+++ Dynamic Firewall/final.py
@@ -10,31 +10,6 @@ from mininet.node import RemoteController
 
 class final_topo(Topo):
 	def build(self):
-		# Examples!
-		# Create a host with a default route of the ethernet interface. You'll need to set the
-		# default gateway like this for every host you make on this assignment to make sure all 
-		# packets are sent out that port. Make sure to change the h# in the defaultRoute area
-		# and the MAC address when you add more hosts!
-		# h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='1.1.1.1/24', defaultRoute="h1-eth0")
-		# h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='2.2.2.2/24', defaultRoute="h2-eth0")
-
-		# Create a switch. No changes here from Lab 1.
-		# s1 = self.addSwitch('s1')
-
-		# Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on 
-		# Host 2. This is representing the physical port on the switch or host that you are 
-		# connecting to.
-		#
-		# IMPORTANT NOTES: 
-		# - On a single device, you can only use each port once! So, on s1, only 1 device can be
-		#   plugged in to port 1, only one device can be plugged in to port 2, etc.
-		# - On the "host" side of connections, you must make sure to always match the port you 
-		#   set as the default route when you created the device above. Usually, this means you 
-		#   should plug in to port 0 (since you set the default route to h#-eth0).
-		#
-		# self.addLink(s1,h1, port1=8, port2=0)
-		# self.addLink(s1,h2, port1=9, port2=0)
-
 		s1 = self.addSwitch('s1')
 		s2 = self.addSwitch('s2')
 		s3 = self.addSwitch('s3')
@@ -59,9 +34,6 @@ class final_topo(Topo):
 		self.addLink(s4,s2, port1=2, port2=1)
 		self.addLink(s4,s3, port1=3, port2=1)
 		self.addLink(s4,s5, port1=5, port2=1)
-
-
-
 
 	def drop(self, packet, packet_in):
 		msg = of.ofp_flow_mod()
@@ -89,7 +61,6 @@ def configure():
 	CLI(net)
   
 	net.stop()
-
 
 if __name__ == '__main__':
   configure()
